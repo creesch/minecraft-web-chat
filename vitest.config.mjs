8ba7adced88fd22cf3741207228fa78c0e55d26d
Daniel Angell <me@danangell.com>
2024-12-20 14:41:32 -0500

    Add tests for message_parsing.mjs

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    environment: "jsdom",
     include: [
         'src/test/resources/web/js/**/*.test.mjs',
         'src/test/resources/web/js/**/*.test.js',
